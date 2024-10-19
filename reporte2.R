82d098b1a76f47115ebdc0729b84c1e185c36b5d
Héctor Alejandro Márquez González <hamgalex@hotmail.com>
2024-10-19 10:20:06 -0600

    Added histograms for each column

--- reporte2.R
+++ reporte2.R
@@ -1,2 +1,87 @@
-# Código para hacer el Reporte 2
+# Reporte 2
+lifeExpectancy <- read.csv("C:/Users/hamga/Documents/maestria/1er/MEB/codigosR/ProyectoMEB/LifeExpectancyData.csv")
+lifeExpectancy
 
+# Omitir filas donde haya valores NA
+lifeExpectancy <- na.omit(lifeExpectancy)
+colSums(is.na(lifeExpectancy))
+
+# Medidas descriptivas
+for (col in colnames(lifeExpectancy)) {
+  cat("\nMedidas descriptivas para columna", col, ":\n")
+  print(summary(lifeExpectancy[[col]]))
+}
+
+# Histogramas
+hist(lifeExpectancy$Life.expectancy,
+     main = "Histograma de Life Expectancy",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     ylim = c(0, 700)
+)
+hist(lifeExpectancy$Adult.Mortality,
+     main = "Histograma de Adult Mortality",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     xlim = c(0,800)
+)
+hist(lifeExpectancy$Alcohol,
+     main = "Histograma de Alcohol",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     xlim = c(0,20),
+     ylim = c(0,700)
+)
+hist(lifeExpectancy$BMI,
+     main = "Histograma de BMI",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black"
+)
+hist(lifeExpectancy$GDP,
+     main = "Histograma de GDP",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     ylim = c(0,2000)
+)
+hist(lifeExpectancy$HIV.AIDS,
+     main = "Histograma de HIV AIDS",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     xlim = c(0,60),
+     ylim = c(0,2500)
+)
+hist(lifeExpectancy$Income.composition.of.resources,
+     main = "Histograma de Income Composition of Resources",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     ylim = c(0,600)
+)
+hist(lifeExpectancy$Schooling,
+     main = "Histograma de Schooling",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black",
+     xlim = c(0,25)
+)
+hist(lifeExpectancy$Total.expenditure,
+     main = "Histograma de Total Expenditure",
+     xlab = "Valores",
+     ylab = "Frecuencia",
+     col = "skyblue",
+     border = "black"
+)
